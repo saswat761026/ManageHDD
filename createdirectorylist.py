71581f77fe38d756f00af7d594c872063f447946
saswat761026 <saswatpanda104@gmail.com>
2021-03-04 17:14:45 +0530

    testing

--- createdirectorylist.py
+++ createdirectorylist.py
@@ -11,12 +11,23 @@ class DirectoryDetails:
         listOfDirectories = [x[0] for x in os.walk("/run/media/avish/Elements/Series/Anime")]
         listOfDirectories.sort(reverse=True)
         filteredList = []
+        counter = 0
+        notMatch = 'Not Matched'
+        # with open('dirList.txt', 'w') as u:
+        #     u.writelines([f'{s}\n' for s in listOfDirectories])
+        f = open('print.txt', 'a+')
+        r = re.compile('^\/([a-zA-Z\/ ]\w+)+.(Season )\d$')
         for dirc in listOfDirectories:
-            s = re.match('^\/([a-zA-Z\/ ]\w+)+.(Season )\d$', dirc)
+            counter = counter + 1
+            s = r.match(dirc)
+            f.write(f'counter: {counter} directory: {dirc}, regex:{s.groups() if s else notMatch}')
             if s:
                 print(s)
                 if dirc not in filteredList:
                     filteredList.append(dirc)
+            if counter>200:
+                break
+        f.close()            
         print(filteredList)
 
 
